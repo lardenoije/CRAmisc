f35cdc83b2243d3cd8effbf80faec6d365539d53
Curtis Alexander <calex@calex.org>
2016-06-18 15:09:43 -0500

    comment all out for time being

--- R/xml.R
+++ R/xml.R
@@ -1,17 +1,17 @@
 # create safe versions
 # different form of error handling
-safe_xml_find_one <- purrr::safely(xml2::xml_find_one, otherwise = NA_character_)
-safe_xml_find_all <- purrr::safely(xml2::xml_find_all, otherwise = NA_character_)
-safe_xml_attr <- purrr::safely(xml2::xml_attr, otherwise = NA_character_)
-safe_xml_text <- purrr::safely(xml2::xml_text, otherwise = NA_character_)
+#safe_xml_find_one <- purrr::safely(xml2::xml_find_one, otherwise = NA_character_)
+#safe_xml_find_all <- purrr::safely(xml2::xml_find_all, otherwise = NA_character_)
+#safe_xml_attr <- purrr::safely(xml2::xml_attr, otherwise = NA_character_)
+#safe_xml_text <- purrr::safely(xml2::xml_text, otherwise = NA_character_)
 
-get_xml_attr <- function(xml_col, xpath, attr_name) {
+#get_xml_attr <- function(xml_col, xpath, attr_name) {
   # using the xpath, find the attribute
-  x <- xml2::read_xml(xml_col)
-  xml_col_node <- safe_xml_find_one(x, xpath, ns = xml2::xml_ns(x))
-  xml_col_attr <- safe_xml_attr(xml_col_node$result, attr_name)
-  return(xml_col_attr$result)
-}
+#  x <- xml2::read_xml(xml_col)
+#  xml_col_node <- safe_xml_find_one(x, xpath, ns = xml2::xml_ns(x))
+#  xml_col_attr <- safe_xml_attr(xml_col_node$result, attr_name)
+  #return(xml_col_attr$result)
+#}
 
 # this is the core function that parses the XML
 # it will be wrapped in another function before operating on the df
@@ -30,32 +30,32 @@ get_xml_attr <- function(xml_col, xpath, attr_name) {
 #   ret_var_name = what should the value being extracted be named
 #   dots (...) = are required so that columns other than the XML column are
 #     passed through the mapping functions
-xml_get_value_ <- function(xml,
-                           xpath,
-                           extract_type,
-                           extract_value,
-                           ret_var_name,
-                           ...) {
+#xml_get_value_ <- function(xml,
+#                           xpath,
+#                           extract_type,
+                           #extract_value,
+                           #ret_var_name,
+                           #...) {
 
-  if (!extract_type %in% c("text", "attr")) {
-    stop("var_name must be either \"text\" or \"attr\"")
-  }
+#  if (!extract_type %in% c("text", "attr")) {
+#    stop("var_name must be either \"text\" or \"attr\"")
+#  }
 
   # read the entire xml into memory
-  xml_doc <- read_xml(xml)
+  #xml_doc <- read_xml(xml)
 
   # find the appropriate node using xpath
-  xml_node <- safe_xml_find_one(xml_doc, xpath, ns = xml_ns(xml_doc))
+#  xml_node <- safe_xml_find_one(xml_doc, xpath, ns = xml_ns(xml_doc))
 
   # extract the actual value
-  if (extract_type == "attr") {
-    xpath_result <- safe_xml_attr(xml_node$result, extract_value)$result
-  } else {
-    xpath_result <- safe_xml_text(xml_node$result)$result
-  }
+#  if (extract_type == "attr") {
+  #  xpath_result <- safe_xml_attr(xml_node$result, extract_value)$result
+  #} else {
+  #  xpath_result <- safe_xml_text(xml_node$result)$result
+  #}
 
   # assign a name to the result
-  names(xpath_result) <- ret_var_name
-
-  return(xpath_result)
-}
+  #names(xpath_result) <- ret_var_name
+#
+#  return(xpath_result)
+#}

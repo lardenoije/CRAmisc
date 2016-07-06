1a306138baabcc78a01c1ff0482d7e01460c7a22
Curtis Alexander <calex@calex.org>
2016-07-06 14:43:21 -0500

    add OS specific tests; able to do so with skip_on_os function

--- tests/testthat/test-os.R
+++ tests/testthat/test-os.R
@@ -6,3 +6,18 @@ test_that("is_os returns logical", {
   expect_type(is_osx(), "logical")
   expect_type(is_windows(), "logical")
 })
+
+test_that("is_windows returns true", {
+  skip_on_os(c("linux", "mac", "solaris"))
+  expect_equal(is_windows(), TRUE)
+})
+
+test_that("is_osx returns true", {
+  skip_on_os(c("linux", "windows", "solaris"))
+  expect_equal(is_osx(), TRUE)
+})
+
+test_that("is_linux returns true", {
+  skip_on_os(c("mac", "windows", "solaris"))
+  expect_equal(is_linux(), TRUE)
+})

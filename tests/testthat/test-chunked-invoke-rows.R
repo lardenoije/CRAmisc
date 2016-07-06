ce884aa704890610769a442f7f57cb96ef8b9eda
Curtis Alexander <calex@calex.org>
2016-07-06 14:40:26 -0500

    add stubs for tests

---
+++ tests/testthat/test-chunked-invoke-rows.R
@@ -0,0 +1,7 @@
+context("Chunked Invoke Rows")
+
+# add tests
+test_that("tests on chunked-invoke-rows.R", {
+  succeed(message = paste0("Skipping tests on chunked-invoke-rows.R.\n",
+                           "Will add tests later."))
+})
---
+++ tests/testthat/test-diff.R
@@ -0,0 +1,7 @@
+context("Dataframe Differences")
+
+# add tests
+test_that("tests on diff.R", {
+  succeed(message = paste0("Skipping tests on diff.R.\n",
+                           "Will add tests later."))
+})
